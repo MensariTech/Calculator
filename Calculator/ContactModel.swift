88b86fd237fa56eff58fdaed8618b090a35a1e02
HamidKiamozafari <59441471+HamidKiamozafari@users.noreply.github.com>
2020-04-30 23:19:30 +0430

    add-contact model

---
+++ Calculator/ContactModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContactModel.swift
+//  Calculator
+//
+//  Created by Hamid on 2/11/1399 AP.
+//  Copyright © 1399 AP Moein Barzegaran. All rights reserved.
+//
+
+import Foundation
+
+struct ContactModel {
+    var name: String?
+}
