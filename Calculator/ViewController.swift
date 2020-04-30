866ce4e3ef8a115481005ded12ff12ca19837825
Ensi Khosravi <khosravi.ensi@gmail.com>
2020-04-30 23:19:56 +0430

    Add button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var button:UIButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
